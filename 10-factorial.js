9e3f91759e802ee5b1063cc019530a93831b3cc8
Gerald Okoth <okothgerald449@gmail.com>
2025-05-21 19:53:33 +0300

    Print factorial of argument passed

--- 10-factorial.js
+++ 10-factorial.js
@@ -3,9 +3,15 @@ const { argv } = require("node:process");
 const firstArg = argv[2];
 const converted = Number(firstArg);
 
-const factorial = (n) => {
+function factorial(n) {
   if (isNaN(n) || n < 0) return 1;
   if (n === 0 || n === 1) return 1;
   return n * factorial(n - 1);
-};
+}
+
+// const factorial = (n) => {
+//   if (isNaN(n) || n < 0) return 1;
+//   if (n === 0 || n === 1) return 1;
+//   return n * factorial(n - 1);
+// };
 console.log(factorial(converted));
